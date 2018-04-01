833b78e93b42ccd03fd5c512d183c4634c3f91be
Michael Pascuzzi <gatorpazz@gmail.com>
2018-04-01 11:03:16 -0400

    Finishing up the flow

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,23 +2,23 @@ var axios = require('axios');
 
 var id = 'e41ce27ed4ec6da04b7f';
 var sec = '263aee645167b679bd24facb14322ebbb2c6127e';
-var params = '?client_id=' + id + '&client_secret' + sec;
+var params = "?client_id=" + id + "&client_secret=" + sec;
 
-function getProfile(username) {
+function getProfile (username) {
   return axios.get('https://api.github.com/users/' + username + params)
-    .then(function(user) {
+    .then(function (user) {
       return user.data;
     });
 }
 
 function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + params + '/repos' + params + '&per_page=100')
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
 }
 
 function getStarCount (repos) {
-  return repos.data.reduce(function(count, repo) {
-    return count + repo.stargazers_count;
-  }, 0)
+  return repos.data.reduce(function (count, repo) {
+    return count + repo.stargazers_count
+  }, 0);
 }
 
 function calculateScore (profile, repos) {
@@ -37,7 +37,7 @@ function getUserData (player) {
   return axios.all([
     getProfile(player),
     getRepos(player)
-  ]).then(function(data) {
+  ]).then(function (data) {
     var profile = data[0];
     var repos = data[1];
 
@@ -45,20 +45,20 @@ function getUserData (player) {
       profile: profile,
       score: calculateScore(profile, repos)
     }
-  })
+  });
 }
 
 function sortPlayers (players) {
-  return players.sort(function(a,b) {
+  return players.sort(function (a,b) {
     return b.score - a.score;
   });
 }
 
 module.exports = {
-  battle: function(players) {
+  battle: function (players) {
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
-      .catch(handlError)
+      .catch(handleError);
   },
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
